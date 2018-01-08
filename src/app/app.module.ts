671fc67c5810cf325608be41fb59143ad6b00758
Bernardo Gamarra <bernardo.gamarra@ec.gba.gov.ar>
2018-01-08 15:05:26 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdministrationModule } from './modules/administration/administration.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AdministrationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
